66dafbf3576531dd47f1a365fdc833afb1bb20fb
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-01-31 13:03:41 -0800

    initial commit

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Default functions template
+ *
+ * This file is used to bootstrap the theme.
+ *
+ * @package   ClassicPress Alpha
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2023. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/classicpress-alpha
+ */
+
+# ------------------------------------------------------------------------------
+# Bootstrap the theme.
+# ------------------------------------------------------------------------------
+#
+# Load the bootstrap files. Note that autoloading should happen first so that
+# any classes/functions are available that we might need.
+
+require_once( get_parent_theme_file_path( 'app/bootstrap-autoload.php' ) );
